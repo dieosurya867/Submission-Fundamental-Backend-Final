4850f2b7e8e4e1cb91cfbc169bf818a3350a0790
dieosurya <dieosurya@gmail.com>
2025-07-10 20:16:31 +0700

    delete file foto

--- migrations/1751198853053_add-coverurl-to-albums.js
+++ migrations/1751198853053_add-coverurl-to-albums.js
@@ -1,11 +1,11 @@
 exports.up = (pgm) => {
-    pgm.addColumn('albums', {
-        cover_url: {
-            type: 'TEXT',
-        },
-    });
+  pgm.addColumn("albums", {
+    cover_url: {
+      type: "TEXT",
+    },
+  });
 };
 
 exports.down = (pgm) => {
-    pgm.dropColumn('albums', 'cover_url');
+  pgm.dropColumn("albums", "cover_url");
 };
